5494c523b2f30ac6a3aecea7b7aca6da0e8e3f26
yosuke.harigae <xoxo18594@gamil.com>
2021-08-11 00:01:09 +0900

    Add Report class

--- src/models/validators/ReportValidator.java
+++ src/models/validators/ReportValidator.java
@@ -7,34 +7,35 @@ import java.util.List;
 import models.Report;
 
 public class ReportValidator {
-    public static List <String> validate(Report r) {
-    	List<String> errors =new ArrayList<String>();
+    public static List<String> validate(Report r) {
+        List<String> errors = new ArrayList<String>();
 
-    	String title_error = _validateTitle(r.getTitle());
-    	if(!title_error.equals("")){
-    		errors.add(title_error);
-    	}
+        String title_error = _validateTitle(r.getTitle());
+        if(!title_error.equals("")) {
+            errors.add(title_error);
+        }
 
-    	String content_error = _validateContent(r.getContent());
-    	if(!content_error.equals("")){
-    		errors.add(content_error);
-    	}
+        String content_error = _validateContent(r.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
 
-    	return errors;
+        return errors;
     }
 
-    	private static String_validateTitle(String title){
-    		if(title == null || title.equals("")){
-    			return "タイトルを入力してください。";
-    		}
-    		return"";
-    	}
-    	private static String _validateContent(String content) {
-            if(content == null || content.equals("")) {
-                return "内容を入力してください。";
-                }
+    private static String _validateTitle(String title) {
+        if(title == null || title.equals("")) {
+            return "タイトルを入力してください。";
+            }
 
-            return "";
+        return "";
     }
 
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "内容を入力してください。";
+            }
+
+        return "";
+    }
 }
